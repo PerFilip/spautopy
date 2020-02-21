ad4833a8dfdfcd4594ffde714562fdac126446a5
Filip Hedén <filipheden@gmail.com>
2020-02-21 19:27:33 +0100

    First functionality test

--- src/app.py
+++ src/app.py
@@ -1,9 +1,19 @@
 import spotipy
+from spotipy import util
+from me import Me
 from spotipy.oauth2 import SpotifyClientCredentials
 
 def run():
-    print("App running!")
-    print("App closing")
+    scope="user-read-currently-playing"
+    user_id, user_secret, user_uri = Me.get_user_credentials()
+    token = util.prompt_for_user_token(scope, user_id, user_secret, user_uri)
+
+    if token:
+        sp = spotipy.Spotify(auth=token)
+        current_track = sp.current_user_playing_track()
+        print(current_track)
+
+    
 
 if __name__=="__main__":
     run()
---
+++ src/me.py
@@ -0,0 +1,8 @@
+class Me:
+
+    @staticmethod
+    def get_user_credentials():
+        client_id = "a53360a2a75944d599c74c10bb7bf340"
+        client_secret = "383d0feb1f044cf59c69fb925c76e52f"
+        client_uri = "http://localhost"
+        return (client_id, client_secret, client_uri)
