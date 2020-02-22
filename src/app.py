4fedbb0a378492eaa94ac8f681e16695b6f0d6c4
Filip Hedén <filipheden@gmail.com>
2020-02-22 11:24:27 +0100

    Exported variables, me.py no longer needed

--- src/app.py
+++ src/app.py
@@ -1,19 +1,16 @@
 import spotipy
 from spotipy import util
-from me import Me
-from spotipy.oauth2 import SpotifyClientCredentials
 
 def run():
     scope="user-read-currently-playing"
-    user_id, user_secret, user_uri = Me.get_user_credentials()
-    token = util.prompt_for_user_token(scope, user_id, user_secret, user_uri)
+    token = util.prompt_for_user_token(scope)
+    print("token = " + token)
 
     if token:
         sp = spotipy.Spotify(auth=token)
+        print("Token provided...")
         current_track = sp.current_user_playing_track()
         print(current_track)
-
-    
 
 if __name__=="__main__":
     run()
