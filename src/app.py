f0c416e26415a8961beb06a011b9e411fdcc109a
Filip Hedén <filipheden@gmail.com>
2020-02-22 12:13:24 +0100

    Gets name correctly

--- src/app.py
+++ src/app.py
@@ -1,16 +1,16 @@
 import spotipy
 from spotipy import util
 
+username = "hedeeen"
+
 def run():
-    scope="user-read-currently-playing"
-    token = util.prompt_for_user_token(scope)
-    print("token = " + token)
+    scope = "user-read-currently-playing"
+    token = util.prompt_for_user_token(username=username, scope=scope)
 
     if token:
         sp = spotipy.Spotify(auth=token)
-        print("Token provided...")
-        current_track = sp.current_user_playing_track()
-        print(current_track)
+        current_track = sp.current_user_playing_track().get("item")
+        print(current_track.get("name"))
 
 if __name__=="__main__":
     run()
