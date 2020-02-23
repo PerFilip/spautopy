621da368ec331de4bb4d7fbdfdded5455912712c
Filip Hedén <filipheden@gmail.com>
2020-02-23 16:48:00 +0100

    Remove liked from Limbo working

--- src/app.py
+++ src/app.py
@@ -1,16 +1,62 @@
-import spotipy
+import spotipy, sys
 from spotipy import util
 
 username = "hedeeen"
+playlist = "4x98GQp3hWLtX4MEAtXWhB" # playlist id
 
-def run():
-    scope = "user-read-currently-playing"
+def main():
+    scope = "user-read-currently-playing playlist-modify-private playlist-modify-public playlist-read-private playlist-read-collaborative user-library-read"
     token = util.prompt_for_user_token(username=username, scope=scope)
 
     if token:
         sp = spotipy.Spotify(auth=token)
-        current_track = sp.current_user_playing_track().get("item")
-        print(current_track.get("name"))
+        if sys.argv[1]=="-add":
+            add_current_track(sp)
+        elif sys.argv[1]=="-remove_liked":
+            limbo_liked = find_liked_in_limbo(sp)
+            remove_from_limbo(sp, limbo_liked)
+
+        else:
+            print("\""+sys.argv[1] + "\" is not a valid option.\n" + "Try one of these instead: -add -remove_liked")
+        
+# Adds currently playing track to Limbo (my choosen playlist)
+def add_current_track(sp):
+    current_track_data = sp.current_user_playing_track()
+    if current_track_data==None:
+        print("No currently playing track.")
+        return
+    current_track = current_track_data.get("item")
+    track_id = current_track.get("id")
+    sp.user_playlist_add_tracks(username, playlist_id=playlist, tracks=[track_id])
+    print("Added \"" + current_track.get("name") + "\" to Limbo.")
+
+def remove_from_limbo(sp, tracks):
+    sp.user_playlist_remove_all_occurrences_of_tracks(user=username, playlist_id=playlist, tracks=tracks)
+
+def find_liked_in_limbo(sp):
+    limbo_liked_tracks = []
+    limbo = sp.playlist_tracks(playlist_id=playlist).get("items")
+    recent_likes = add_all_liked_tracks(sp)
+    for track in limbo:
+        limbo_track_id = track.get("track").get("id")
+        for liked_track in recent_likes:
+            if (liked_track.get("track").get("id")==limbo_track_id):
+                limbo_liked_tracks.append(limbo_track_id)
+    return limbo_liked_tracks
+
+def add_all_liked_tracks(sp):
+    recent_likes = []
+    liked_tracks = sp.current_user_saved_tracks(limit=20).get("items")
+    for i in range(5):
+        for track in liked_tracks:
+            recent_likes.append(track)
+        liked_tracks = sp.current_user_saved_tracks(limit=20, offset=20*i).get("items")
+    return recent_likes
+        
+def add_to_next_month_playlist(sp):
+    current_track = sp.current_user_playing_track().get("item")
+
+
 
 if __name__=="__main__":
-    run()
+    main()
